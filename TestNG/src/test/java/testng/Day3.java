6753c07c2d86d7257db6134a8608a49089201ff9
manish021085 <manishksingh0210@gmail.com>
2020-10-15 22:33:14 +0530

    new change

--- TestNG/src/test/java/testng/Day3.java
+++ TestNG/src/test/java/testng/Day3.java
@@ -1,5 +1,23 @@
 package testng;
 
+import org.testng.annotations.Test;
+
 public class Day3 {
+	
+	@Test
+	public void webLoginCarLoan() {
+		System.out.println("webLoginCarLoan");
+	}
+	
+	@Test
+	public void mobileLoginCarLoan() {
+		System.out.println("mobileLoginCarLoan");
+	}
+	
+	@Test
+	public void loginAPIcarLoan() {
+		System.out.println("APIcarLoan");
+	}
+	
 
 }
--- TestNG/src/test/java/testng/Day4.java
+++ TestNG/src/test/java/testng/Day4.java
@@ -1,5 +1,22 @@
 package testng;
 
+import org.testng.annotations.Test;
+
 public class Day4 {
+	
+	@Test
+	public void webLoginHomeLoan() {
+		System.out.println("webLoginHomeLoan");
+	}
+	
+	@Test
+	public void mobileLoginHomeLoan() {
+		System.out.println("mobileLoginHomeLoan");
+	}
+	
+	@Test
+	public void loginAPIHomeLoan() {
+		System.out.println("APIloginHomen");
+	}
 
 }
